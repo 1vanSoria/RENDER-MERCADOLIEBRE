ad1d280963865aef22f6862f4160db609473086b
1vanSoria <o.ivansoria@gmail.com>
2022-10-25 22:14:25 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express= require('express');
+const { dirname } = require('path');
+
+const app= express();
+const path = require ('path');
+
+
+app.use("/static", express.static("public"));
+
+app.listen(3000, ()=>{
+    console.log('servidor funcionando');
+});
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.resolve("./views/home.html"));
+});
+
+app.get("/register", (req, res)=>{
+
+    res.sendFile(path.resolve("./views/register.html"));
+});
+app.get("/login", (req, res)=>{
+
+    res.sendFile(path.resolve("./views/login.html"));
+});
+
